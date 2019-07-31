992606bbd72578b37140121d041a5b0cec978c81
Kevin-S <kevsilvo@gmail.com>
2019-07-31 18:18:45 -0400

    added dependencies

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+// needed dependancies:
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  applyMiddleware(logger)
   /* applyMiddleware goes here */
 );
 
